d6f2d6d7611a6b9c4823843441bb2e1f03dfc75c
Mabel Bernal Amaya <mabel.bernal-a@mail.escuelaing.edu.co>
2026-06-11 18:30:18 -0500

    ejercicio 1 semana 1 numeros pares amyores a diez

--- src/main/dosw/semana_1/streams/Ejercicio1.java
+++ src/main/dosw/semana_1/streams/Ejercicio1.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 public class Ejercicio1 {
     public static void main(String[] args) {
         List<Integer> numeros = List.of(3, 8, 10, 12, 15, 18, 20);
-        
+
         List<Integer> resultado = numeros.stream()
             .filter(n -> n % 2 == 0)
             .filter(n -> n > 10)
             .collect(Collectors.toList());
-        
+
         System.out.println("Números pares mayores a 10: " + resultado);
     }
 }
