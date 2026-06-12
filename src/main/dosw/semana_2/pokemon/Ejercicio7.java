55dee263b6991964e0c65c10bf2ea601dbcc15b8
Mabel Bernal Amaya <mabel.bernal-a@mail.escuelaing.edu.co>
2026-06-11 20:19:18 -0500

    subiendo ejercicio 7 pokemon

---
+++ src/main/dosw/semana_2/pokemon/Ejercicio7.java
@@ -0,0 +1,20 @@
+package main.dosw.semana_2.pokemon;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        List<String> pokemones = Arrays.asList(
+            "Squirtle", "Pikachu", "Mewtwo",
+            "Bulbasaur", "Charmander", "Abra"
+        );
+
+        List<String> ordenados = pokemones.stream()
+            .sorted()
+            .collect(Collectors.toList());
+
+        System.out.println("Pokedex ordenada: " + ordenados);
+    }
+}
